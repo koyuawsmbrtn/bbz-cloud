9dbd79272fa1f8d07fdec69849c74b5c82825625
Dennis Clausen <dennis.clausen@bbz-rd-eck.de>
2022-08-29 16:04:35 +0200

    new approach

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,17 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron');
+require('./browser-mediadevices');
+const getDisplayMedia = require('./browser-mediadevices');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('api', {
   send: (channel, data) => {
     // whitelist channels
-    const validChannels = ['autostart', 'zoom'];
+    const validChannels = ['autostart', 'zoom', 'desktopCapturerGetSources'];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     }
   },
   receive: (channel, func) => {
-    const validChannels = ['fromMain'];
+    const validChannels = ['fromMain', 'desktopCapturerGetSources'];
     if (validChannels.includes(channel)) {
       // Deliberately strip event as it includes `sender`
       ipcRenderer.on(channel, (event, ...args) => func(...args));
