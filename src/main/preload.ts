a96d60a9bd7bf268d6839f207a5e56ffd61632f6
Dennis Clausen <dennis.clausen@bbz-rd-eck.de>
2022-09-05 12:49:17 +0200

    github.com/aabuhijleh/override-getDisplayMedia

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,6 +1,8 @@
 /* eslint-disable promise/catch-or-return */
 /* eslint-disable import/prefer-default-export */
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer, desktopCapturer } = require('electron');
+const { readFileSync } = require('fs');
+const { join } = require('path');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
@@ -30,4 +32,23 @@ contextBridge.exposeInMainWorld('api', {
       ipcRenderer.on(channel, (event, ...args) => func(...args));
     }
   },
+});
+
+// inject renderer.js into the web page
+window.addEventListener('DOMContentLoaded', () => {
+  const rendererScript = document.createElement('script');
+  rendererScript.text = readFileSync(join(__dirname, 'renderer.js'), 'utf8');
+  document.body.appendChild(rendererScript);
+});
+
+contextBridge.exposeInMainWorld('myCustomGetDisplayMedia', async () => {
+  const sources = await desktopCapturer.getSources({
+    types: ['window', 'screen'],
+  });
+
+  // you should create some kind of UI to prompt the user
+  // to select the correct source like Google Chrome does
+  const selectedSource = sources[0]; // this is just for testing purposes
+
+  return selectedSource;
 });
