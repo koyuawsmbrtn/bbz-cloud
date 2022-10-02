fb87c17dfb56c747c310f73f799b93daca0c8d15
koyu <me@koyu.space>
2022-10-02 04:30:53 +0200

    Finish build script

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
+source ~/.config/user-dirs.dirs
 git pull
-yarn dist
+git checkout -- .
+yarn
 VERSION="$(jq .version package.json)"
-BASEDIR="~/Schreibtisch/bbzcloud-build"
+BASEDIR="$XDG_DESKTOP_DIR/bbzcloud-build"
 if [ -d "$BASEDIR" ]; then
   # Take action if $DIR exists. #
   echo "${DIR} existiert bereits. Exit!"
-  exit
+  exit 1
 fi
 mkdir $BASEDIR
+rm -rf release/build
+echo "Baue Lehrerversion..."
+yarn dist -lw # Only Windows and Linux for now
+mkdir $BASEDIR/teacher
+mv release/build $BASEDIR/teacher
+echo "Baue Schülerversion..."
+echo "{ \"value\": false }" > assets/isTeacher.json
+sed -i -e 's/bbz-cloud/bbz-cloud-sus/g' package.json
+sed -i -e 's/koyuawsmbrtn/dclausen01/g' package.json
+yarn dist -lw
+mkdir $BASEDIR/sus
+mv release/build $BASEDIR/sus
+git checkout -- .
+echo "Fertig! 🎉️"
