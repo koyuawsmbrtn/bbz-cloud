18ab99942e262621b3f1cb33b95220d4ff6cbd82
Dennis Clausen <dennis.clausen@bbz-rd-eck.de>
2023-01-25 16:25:48 +0100

    minWidth and minHeight for mainWindow

--- src/main/main.ts
+++ src/main/main.ts
@@ -206,6 +206,8 @@ const createWindow = async () => {
     show: false,
     width: 1600,
     height: 900,
+    minWidth: 680,
+    minHeight: 680,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
