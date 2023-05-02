0c3f5bcd0209b42e4a5e47d5de2d5a02d47db24d
Leonie Ain <koyu@petra.local>
2023-05-02 09:23:31 +0000

    Try adding a menu for macOS to fix clipboard issue

--- src/main/main.ts
+++ src/main/main.ts
@@ -223,7 +223,97 @@ const createWindow = async () => {
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
 
-  mainWindow.setMenu(null);
+  var mainWindowMenuTemplate = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo',
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall',
+        },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload();
+          },
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function () {
+            if (process.platform == 'darwin') return 'Ctrl+Command+F';
+            else return 'F11';
+          })(),
+          click: function (item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          },
+        },
+      ],
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize',
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close',
+        },
+      ],
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: function () {
+            require('electron').shell.openExternal('http://electron.atom.io');
+          },
+        },
+      ],
+    },
+  ];
 
   mainWindow.webContents.send('resize', mainWindow.getBounds().height);
 
@@ -357,7 +447,59 @@ const createWindow = async () => {
     mainWindow = null;
   });
 
-  mainWindow.setMenu(null);
+  if (process.platform == 'darwin') {
+    var name = require('electron').app.getName();
+    // I hope this doesn't crash :)
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          type: 'separator',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          role: 'hide',
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers',
+        },
+        {
+          label: 'Show All',
+          role: 'unhide',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function () {
+            app.quit();
+          },
+        },
+      ],
+    });
+    // Window menu.
+    template[3].submenu.push(
+      {
+        type: 'separator',
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front',
+      }
+    );
+
+    mainWindow.setMenu(Menu.buildFromTemplate(mainWindowMenuTemplate));
+  } else {
+    mainWindow.setMenu(null);
+  }
 
   // Open urls in the user's browser - just as fallback, should never be useds
   mainWindow.webContents.setWindowOpenHandler((edata) => {
