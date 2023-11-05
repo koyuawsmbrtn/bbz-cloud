904bed9fe2950a9055fea9ce25c8e1524111938e
koyu <me@koyu.space>
2023-11-05 20:55:39 +0100

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "This will only deploy .deb and .rpm packages. Please manually deploy for pacman and snap."
+source ~/.config/user-dirs.dirs
 BASEDIR="$XDG_DESKTOP_DIR/bbzcloud-build"
 fury migrate $BASEDIR/sus/build/*.deb $BASEDIR/sus/build/*.rpm
